21712871bd704b02818268c6d6aedb75e34a7e47
Giovanni Sakti <giosakti@gmail.com>
2018-05-23 05:28:20 +0000

    [gio] don't apply sysctl recipe for now

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,10 @@
 include_recipe 'base_cookbook::_hostname'
 include_recipe 'base_cookbook::_packages'
 include_recipe 'base_cookbook::_limits'
-include_recipe 'base_cookbook::_sysctl'
+
+# TODO: should investigate what's working and what's not for sysctl in container
+#include_recipe 'base_cookbook::_sysctl'
+
 include_recipe 'base_cookbook::_ntp'
 include_recipe 'base_cookbook::_gate' if node['gate']['enabled']
 include_recipe 'sudo'
